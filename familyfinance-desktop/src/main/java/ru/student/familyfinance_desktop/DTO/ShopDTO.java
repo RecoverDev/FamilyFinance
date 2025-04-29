026cad87fc4c203964915a4a0a3e28910693757d
RecoverDev <recover@list.ru>
2025-04-30 00:43:07 +0300

    Добавлены ShopDTO и ShopMapper

---
+++ familyfinance-desktop/src/main/java/ru/student/familyfinance_desktop/DTO/ShopDTO.java
@@ -0,0 +1,21 @@
+package ru.student.familyfinance_desktop.DTO;
+
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class ShopDTO {
+    private SimpleLongProperty id;
+    private SimpleStringProperty name;
+
+    public ShopDTO(long id, String name) {
+        this.id = new SimpleLongProperty(id);
+        this.name = new SimpleStringProperty(name);
+    }
+
+    public long getId() { return this.id.get(); }
+    public void setId(long value) { this.id.set(value); }
+
+    public String getName() { return this.name.get(); }
+    public void setName(String value) { this.name.set(value); }
+
+}
---
+++ familyfinance-desktop/src/main/java/ru/student/familyfinance_desktop/Mapper/ShopMapper.java
@@ -0,0 +1,29 @@
+package ru.student.familyfinance_desktop.Mapper;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ru.student.familyfinance_desktop.DTO.ShopDTO;
+import ru.student.familyfinance_desktop.Model.Person;
+import ru.student.familyfinance_desktop.Model.Shop;
+
+@Mapper(componentModel = SPRING)
+public abstract class ShopMapper {
+
+    @Autowired
+    protected Person person;
+
+    public abstract ShopDTO toShopDTO(Shop shop);
+
+    public abstract List<ShopDTO> toListShopDTO(List<Shop> shops);
+
+    @Mapping(target = "person_id", expression = "java(person.getId())")
+    public abstract Shop toShop(ShopDTO shopDTO);
+
+    public abstract List<Shop> toListShop(List<ShopDTO> shopsDTO);
+}
